63e06a45a459a42e89fff474dfc293566ffc9fd0
dwicao <lutfiandwica@gmail.com>
2017-05-12 08:36:20 +0700

    enable store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-// import configureStore from '../configureStore';
+import configureStore from '../store';
 import App from './components/App';
 
-// const store = configureStore();
-const store = null;
+const store = configureStore();
 
 export default class Root extends Component {
   render() {
